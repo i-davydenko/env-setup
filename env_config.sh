3ea32454870970832d65a22a6ce9a4e356ec4913
Ivan <ivan.davydenko.ua@gmail.com>
2020-11-13 19:33:56 +0200

    додав скрипт для простішого запуску fortivpn

--- env_config.sh
+++ env_config.sh
@@ -5,7 +5,10 @@ sudo make install -C ./st
 sudo make install -C ./dmenu
 sudo make install -C ./slstatus
 
-# create symp link to produce 
+# copy config files
+cp ./other_configs/* ~/.config
+
+# create sym link to produce 
 sudo ln -sf ~/Telegram/Telegram /usr/local/bin/tg
 
 sudo cp ./dwm-run /usr/bin/
