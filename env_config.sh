90c0390a226908072978813be8f9ff42fb6ae2ec
IvanD <ivan.davydenko.ua@gmail.com>
2021-03-19 00:03:09 +0200

    покращення в env_script

--- env_config.sh
+++ env_config.sh
@@ -5,11 +5,16 @@ sudo make install -C ./st
 sudo make install -C ./dmenu
 sudo make install -C ./slstatus
 
-# copy config files
-cp ./other_configs/* ~/.config
-
-# create sym link to produce 
-sudo ln -sf ~/Telegram/Telegram /usr/local/bin/tg
-
+#install dwm enviroment
 sudo cp ./dwm-run /usr/bin/
 sudo cp ./dwm-run.desktop /usr/share/xsessions/
+
+# copy config files
+cp ./other_configs/* ~/.config/
+sudo cp ./scripts/* /usr/bin/
+
+# create sym link to produce 
+sudo ln -sf ~/Telegram/Telegram /usr/bin/tg
+sudo ln -sf ~/chromium-latest-linux/run.sh /usr/bin/chrm
+sudo ln -sf ~/downloads/Discord/Discord /usr/bin/Discord
+
